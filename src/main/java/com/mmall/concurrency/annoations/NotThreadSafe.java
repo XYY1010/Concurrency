bec52c5b03b2dbf6c30d0560142c079d9d3cbd0c
徐杨杨 <742046560@qq.com>
2019-04-22 22:12:50 +0800

    add annoations

---
+++ src/main/java/com/mmall/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,17 @@
+package com.mmall.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author XYY
+ * @date 2019-04-22 22:05
+ * 课程里用来标记【线程不安全】的类或者写法
+ */
+@Target(ElementType.TYPE)       // 作用目标
+@Retention(RetentionPolicy.SOURCE)                    // 注解存在的范围
+public @interface NotThreadSafe {
+    String value() default "";
+}
